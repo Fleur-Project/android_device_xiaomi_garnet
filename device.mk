941457939d2d83f72b15a3c097ee9901d1a0aa04
Alhaidar Latif <ziyalhaidar@gmail.com>
2026-08-25 17:02:42 +0700

    garnet: Implement torch light control
    [RealJohnGalt: Modify for oneplus usage and add toggle switch]
    [cyberknight777/ralph950412: Adapt nodes and SEPolicy for Xiaomi SM8350]
    [rk134: Adapt nodes/sepolicy for Xiaomi SM8450]
    
    Change-Id: Icd32d1f6aedb55462c9df4d7cc63a2a4c4e4263e
    Signed-off-by: ralph950412 <ralph950412@gmail.com>
    Signed-off-by: rk134 <rahul@aospa.co>
    
    sm8450-common: camera: Implement supportsSetTorchModeExt
    
    Change-Id: I2ec2f4a30723763e6123a1b742468752f38e3d2f
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    sm8450-common: camera: Add enabled bool for finished workarounds
    
    Change-Id: Iede122113f17789cdf88896cb32bc30f574ec54f
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    sm8450-common: sepolicy: Address a cameraserver neverallow
    
    - For some reason, allowing cameraserver to access sysfs_leds on lahaina results in a neverallow.
    - Hence, allow cameraserver to access a new type, sysfs_torch which only accesses the relevant nodes we utilize to alleviate the neverallow.
    
    Change-Id: I8625b32f2bb501bbf85f0c026dca22a8e0bcc939
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: rk134 <rahul@aospa.co>
    
    sm8450-common: rootdir: Label brightness node correctly
    
    - We only utilize switch_2/brightness as a toggle switch so explicitly label it.
    
    Change-Id: Iaaab045e7f01e88cd17c00e3e92463d419d65dcc
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: ralph950412 <ralph950412@gmail.com>
    
    sm8450-common: sepolicy: Allow cameraserver to search directories in sysfs_leds
    
    - This fixes a SEPolicy denial.
    
    Change-Id: Ie0124422e3b796a3a172a35a67e00ed44691497b
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    sm8450-common: sepolicy: Allow hal_camera_default to access sys_thermal_flash_state
    
    Co-authored-by: ralph950412 <ralph950412@gmail.com>
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    Co-authored-by: Cyber Knight <cyberknight755@gmail.com>
    Co-authored-by: RealJohnGalt <RealJohnGalt@users.noreply.github.com>
    Signed-off-by: pabloescobar-reborn <pabloescobar-reborn@users.noreply.github.com>
    
    garnet: Adapt nodes/SEPolicy for Xiaomi SM7435
    
    Signed-off-by: Alhaidar Latif <ziyalhaidar@gmail.com>
    
    garnet: Fix broken camera flash when torch control triggered
    
    - Correct sysfs node permissions and ownership to cameraserver:camera so torch brightness slider works.
    - Restore kernel hardware triggers (switch2_trigger & torch0_trigger) on torch shut-off so camera app flash is not broken.
    - Label sys_thermal_flash_state and allow hal_camera_default and cameraserver access.
    
    Signed-off-by: Alhaidar Latif <ziyalhaidar@gmail.com>
    
    Co-authored-by: RealJohnGalt <RealJohnGalt@users.noreply.github.com>
    Co-authored-by: Cyber Knight <cyberknight755@gmail.com>
    Co-authored-by: ralph950412 <ralph950412@gmail.com>
    Co-authored-by: rk134 <rahul@aospa.co>
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    Co-authored-by: pabloescobar-reborn <pabloescobar-reborn@users.noreply.github.com>
    Signed-off-by: Alhaidar Latif <ziyalhaidar@gmail.com>

--- device.mk
+++ device.mk
@@ -123,6 +123,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor
 
+$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.xiaomi_sm7435)
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
