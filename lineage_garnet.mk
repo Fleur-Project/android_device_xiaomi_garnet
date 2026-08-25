869103ca066f01184d2173e47baa73b7c60966d9
Alhaidar Latif <ziyalhaidar@gmail.com>
2026-08-25 00:28:52 +0000

    garnet: Update from OS3.0.302.0.WNRMIXM
    Signed-off-by: Alhaidar Latif <ziyalhaidar@gmail.com>

--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -25,8 +25,8 @@ PRODUCT_SYSTEM_NAME := garnet_global
 PRODUCT_SYSTEM_DEVICE := garnet
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="garnet_global-user 16 BP2A.250605.031.A3 OS3.0.301.0.WNRMIXM release-keys" \
-    BuildFingerprint=Redmi/garnet_global/garnet:16/BP2A.250605.031.A3/OS3.0.301.0.WNRMIXM:user/release-keys \
+    BuildDesc="garnet_global-user 16 BP2A.250605.031.A3 OS3.0.302.0.WNRMIXM release-keys" \
+    BuildFingerprint=Redmi/garnet_global/garnet:16/BP2A.250605.031.A3/OS3.0.302.0.WNRMIXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
 
